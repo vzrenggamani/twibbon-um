cef0e8acf0302ddc8f28e6e4bac248327e2882eb
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2020-09-07 04:29:32 +0700

    FEAT!(template-engine): #1
    * fix(domain): fix typos on domain name
    
    * feat!(handlebars): Use hbs for twibbon selection
    
    * add(twibbon): add test twibbon file
    
    * chore(generator): split generator script to jsfile
    
    * fix!(package): add express hbs and metadata
    
    * chore(generator): delete unused variables

--- index.js
+++ index.js
@@ -2,21 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const exphbs = require('express-handlebars');
 const http = require('http');
 
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'src'));
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
 app.set('view engine', 'hbs');
 app.use('/assets', express.static('assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 setInterval(() => {
-  http.get(`http://twibbon-um.reng.my.id/`);
+  http.get(`https://twibbon-um.reng.my.id/`);
 }, 250000);
 
 app.get('/', (req, res) => {
   console.log(Date.now() + ' Ping Received');
-  res.status(200).render('index');
+  res.status(200).render('index', {
+    twibbon: [
+      {
+        name: 'Test Twibbon',
+        image: 'testtwibbon'
+      },
+      {
+        name: 'Ordinary twibbon',
+        image: 'ordinarytwibbon'
+      }
+    ]
+  });
 });
 
 app.listen(3000, console.log('Listening on port 3000!'));
